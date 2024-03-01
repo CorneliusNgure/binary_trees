c27ee296126813d0d76cc2c31b8045f2887f79ff
CorneliusNgure <kingconelius07@gmail.com>
2024-02-29 19:38:25 -0500

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,12 +9,10 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree != NULL && tree->parent != NULL)
-    {
-        return 1 + binary_tree_depth(tree->parent);
-    }
-    else
-    {
-        return 0;
-    }
+	if (tree != NULL && tree->parent != NULL)
+	{
+		return (1 + binary_tree_depth(tree->parent));
+	}
+	else
+		return (0);
 }
